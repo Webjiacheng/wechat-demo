100d2fb7db33574205d094f6719c0f586190279b
胡嘉成 <13539995474@163.com>
2017-04-13 18:35:09 +0800

    微信demo-4-13

---
+++ common/countheight.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/4/12 0012.
+ */
+$(function () {
+    var h = window.screen.height;
+    $("body").css("height",h);
+    var th=h*0.9;
+    $('.mask').css("height",th);
+    $('.mask-box').css("height",h);
+})
