5a1643b9e85252fd6c6f06e30a438c90e62d7bcd
胡嘉成 <13539995474@163.com>
2017-04-15 18:02:07 +0800

    有马车装公众号完成第一版

--- common/countheight.js
+++ common/countheight.js
@@ -2,9 +2,16 @@
  * Created by Administrator on 2017/4/12 0012.
  */
 $(function () {
+    //求出屏幕高度及设置body高度
     var h = window.screen.height;
     $("body").css("height",h);
+    //设置遮罩层的高度
     var th=h*0.9;
     $('.mask').css("height",th);
     $('.mask-box').css("height",h);
+    //判断是否要固定footer
+    if(h>=667){
+        $('.footer').css('position','absolute');
+        $('.footer').css('bottom',0);
+    }
 })
